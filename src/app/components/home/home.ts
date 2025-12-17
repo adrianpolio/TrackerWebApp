4e456b0779699a9bb8ac6b1a0e9068c1c52e3618
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-16 20:15:20 -0500

    Added home and routed. 1st iteration

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+
+export class Home {
+	constructor(private router: Router) {}
+
+	goTo(route: string) {
+		this.router.navigate([route]);
+	}
+}
