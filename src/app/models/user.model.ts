c250cd7606dc0c4768b81dab5624c331a8a7a5c7
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-20 19:18:06 -0500

    Added user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,24 @@
+export interface User {
+	userId: string;
+	passwordHash: string;
+	name: string;
+	email: string;
+	isActive: boolean;
+	role: string;
+}
+
+export interface CreateUser {
+	passwordHash: string;
+	name: string;
+	email: string;
+    isActive: boolean;
+	role: string;
+}
+
+export interface UpdateUser {
+	passwordHash: string;
+	name: string;
+	email: string;
+	isActive: boolean;
+	role: string;
+}
